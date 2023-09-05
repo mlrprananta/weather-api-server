cfa0bd6c943ce46c3c4de5516b4d7334c7a5daff
mlrprananta <lukeprananta@gmail.com>
2023-09-05 10:57:40 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -8,4 +8,4 @@ export type { RouterContext } from "https://deno.land/x/oak@v12.6.0/mod.ts";
 export { getQuery } from "https://deno.land/x/oak@v12.6.0/helpers.ts";
 export { Status } from "https://deno.land/std@0.196.0/http/mod.ts";
 export { load } from "https://deno.land/std@0.196.0/dotenv/mod.ts";
-export { GeolocationCache } from "https://deno.land/x/geocache@v0.0.2/mod.ts";
+export { GeolocationCache } from "https://deno.land/x/geocache@v0.0.3/mod.ts";
